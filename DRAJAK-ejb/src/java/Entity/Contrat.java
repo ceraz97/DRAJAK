17f838be0c6019a2588c2637daab59213355ba58
Clement Ratz <clementratz@gmail.com>
2020-03-10 15:46:37 +0100

    Mise a jour des classes pour héritage

--- DRAJAK-ejb/src/java/Entity/Contrat.java
+++ DRAJAK-ejb/src/java/Entity/Contrat.java
@@ -15,6 +15,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
@@ -24,7 +26,9 @@ import javax.persistence.Temporal;
  *
  * @author clementratz
  */
+
 @Entity
+@Inheritance (strategy=InheritanceType.TABLE_PER_CLASS)
 public abstract class Contrat implements Serializable {
 
     @OneToMany(mappedBy = "cleContrat")
--- DRAJAK-ejb/src/java/Facades/ContratIndividuelFacade.java
+++ DRAJAK-ejb/src/java/Facades/ContratIndividuelFacade.java
@@ -29,4 +29,6 @@ public class ContratIndividuelFacade extends AbstractFacade<ContratIndividuel> i
         super(ContratIndividuel.class);
     }
     
+    
+    
 }
