a73f695480495c56f4f07393be2ca94118af52ab
Novelzreal <Novelzreal@192.168.144.112>
2020-03-12 13:37:00 +0100

    ClientSession Authentif et Changement mdp

--- DRAJAK-ejb/src/java/Session/ClientSession.java
+++ DRAJAK-ejb/src/java/Session/ClientSession.java
@@ -5,7 +5,14 @@
  */
 package Session;
 
+import Entity.CompteAssure;
+import Facades.CompteAssureFacade;
+import Facades.ContratFacade;
+import Facades.ParticulierFacade;
+import Facades.TransactionFacade;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
+
 
 /**
  *
@@ -13,7 +20,43 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class ClientSession implements ClientSessionLocal {
+    @EJB
+    private ParticulierFacade particulierFacade;
 
+    @EJB
+    private CompteAssureFacade compteAssureFacade;
+
+    @EJB
+    private ContratFacade contratFacade;
+
+    @EJB
+    private TransactionFacade transactionFacade;
+     
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
+    
+     public CompteAssure AuthentificationAssure(String login, String mdp) {
+        CompteAssure SessConnexion;
+        
+        SessConnexion=compteAssureFacade.AuthentifierCompteAssure(login, mdp);
+        
+        return SessConnexion;
+    }
+     
+     public String ChangementMdp(String login, String newMdp, CompteAssure SessConnexion) {
+        CompteAssure ca;
+        String message = null;
+        ca = compteAssureFacade.RechercherCompteAssure(login);
+        
+        if(SessConnexion == ca){
+            if(newMdp.length()>6){
+                compteAssureFacade.ModifierCompteAssure(ca);
+                message = "Le mot de passe a été modifié avec succès.";
+            }
+            else message = "Mot de passe trop court, merci d'entrer un mot de passe de plus de 6 caractères.";
+        }
+        else message = "Le login est incorrect. Le mot de passe n'a pas été modifié.";
+        return message;
+    }
+    
 }
