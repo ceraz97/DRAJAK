27159d39195e6870a06aafcd8b46ec4a377aa379
Novelzreal <Novelzreal@LAPTOP-PBH0P0EE.ad.univ-lyon3.fr>
2020-03-13 11:48:05 +0100

    AssureSession - Rajout CreationDevis et CreationContratIndividuel (à terminer) Contrat individuel - Modification des méthodes PersonnePublique

--- DRAJAK-ejb/src/java/Entity/PersonnePhysique.java
+++ DRAJAK-ejb/src/java/Entity/PersonnePhysique.java
@@ -45,6 +45,22 @@ public abstract class PersonnePhysique implements Serializable {
     private String email;
     @Column (nullable=false)
     private StatutPersonne statutPersonne;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public StatutPersonne getStatutPersonne() {
+        return statutPersonne;
+    }
+
+    public void setStatutPersonne(StatutPersonne statutPersonne) {
+        this.statutPersonne = statutPersonne;
+    }
     
 
     public StatutPersonne getStatutPeronne() {
